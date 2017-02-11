7949c35d3ad4ac20aa7c340390ee188e672569a4
Tasarinan <admin@hbsw.io>
2017-02-11 14:36:34 +0800

    change gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,16 @@ var gulp = require('gulp');
 var del = require('del');
 var concat = require('gulp-concat');
 var replace = require('gulp-replace');
-var exec = require('child_process').exec;
 
-gulp.task('css-app', function() {
-    return gulp.src([
-        'lib/normalize.custom.css',
-        'lib/emojify-base.min.css',
-        'lib/emojify-emoticons.min.css',
-    ])
-})
+gulp.task('testConcat', function() {
+    gulp.src(['F:/英语听力入门1/*.txt',
+            'F:/英语听力入门2/*.txt',
+            'F:/英语听力入门3/*.txt',
+            'F:/英语听力入门4/*.txt',
+        ])
+        .pipe(concat('english.txt')) //合并后的文件名
+        .pipe(gulp.dest('dist/'));
+
+});
+
+gulp.task('default', ['testConcat']);
